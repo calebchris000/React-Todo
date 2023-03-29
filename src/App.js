25acee433fee1823bb4530e3bcb7a604f4a455f8
calebchris000 <calebchris000@gmail.com>
2023-03-29 10:10:45 +0100

    Add app back

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/header';
+import Todo from './components/Todo';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Todo />
+    </div>
+
+  );
+}
+
+export default App;
